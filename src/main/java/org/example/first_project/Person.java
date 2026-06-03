15a0a586db1a83a45f5fe78489a5ae66e4854c92
prosticel152-pixel <prosticel152@gmail.com>
2026-06-03 15:51:43 +0600

    Spring Boot + PostgreSQL

---
+++ src/main/java/org/example/first_project/Person.java
@@ -0,0 +1,44 @@
+package org.example.first_project;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    public Person() {
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
